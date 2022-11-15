2b53c47063da71b328b27519dccf2143c936bafe
Wetzel402 <wetzelredistribution@gmail.com>
2022-11-15 14:54:48 -0600

    deleted:    custom_components.json 	modified:   custom_components/cozi/const.py 	modified:   custom_components/cozi/manifest.json 	deleted:    info.md

--- custom_components/cozi/const.py
+++ custom_components/cozi/const.py
@@ -5,10 +5,10 @@ from datetime import timedelta
 
 LOGGER = logging.getLogger(__package__)
 
-VERSION = '1.0.0'
+VERSION = '2022.11.0'
 DOMAIN = "cozi"
 ATTRIBUTION = "Data provided by cozi.com"
-HELP_URL = "https://www.home-assistant.io/components/cozi"
+HELP_URL = "https://github.com/Wetzel402/hass-cozi"
 
 UPDATE_INTERVAL = timedelta(seconds=300)
 
