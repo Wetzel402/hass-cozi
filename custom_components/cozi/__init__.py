42059c40b059069e2680405c0259d550595f96cf
Wetzel402 <wetzelredistribution@gmail.com>
2022-12-14 16:31:09 -0600

    modified:   custom_components/cozi/__init__.py

--- custom_components/cozi/__init__.py
+++ custom_components/cozi/__init__.py
@@ -76,7 +76,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     hass.data[DOMAIN]["coordinator"] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    #await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(update_listener))
     # await add_services(hass)
